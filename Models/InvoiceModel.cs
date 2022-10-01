753f5cda07674951c353d1ab8be14db52fd70410
Schecher_1 <Schecher_1@web.de>
2022-10-01 22:18:44 +0200

    generate random entitys

---
+++ Models/InvoiceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InvoicesManager.Models
+{
+    public class InvoiceModel
+    {
+        public string OpenInvoiceText { get; } = "Öffnen";
+        public string Reference { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string Organization { get; set; }
+        public DateTime ExhibitionDate { get; set; }
+        public string Path { get; set; }
+    }
+}
--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InvoicesManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,63 @@ using System.Windows.Shapes;
 
 namespace InvoicesManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            GenerateDebugDataRecords();
+#endif
+        }
+
+        private void GenerateDebugDataRecords()
+        {
+            Random r = new Random();
+            string[] sampleOrganization = { "UPS", "MCDonalds", "Telekom", "DHL", "Amazon", "Apple", "Microsoft", "Google", "Facebook", "Twitter" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                InvoiceModel invoice = new InvoiceModel();
+                invoice.Reference = "REF" + r.Next(1000, 9999);
+                invoice.InvoiceNumber = "INV" + r.Next(1000, 9999);
+                invoice.Organization = sampleOrganization[r.Next(0, sampleOrganization.Length)];
+                invoice.ExhibitionDate = DateTime.Now.AddDays(r.Next(0, 100));
+                invoice.Path = "C:\\Invoices\\" + invoice.Reference + ".pdf";
+                
+                DG_Invoices.Items.Add(invoice);
+            }
+        }
+
+
+        private void Bttn_InvoiceAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bttn_InvoiceEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bttn_InvoiceRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bttn_Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bttn_About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void DG_Invoices_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            object sad = DG_Invoices.SelectedItem;
         }
     }
 }
