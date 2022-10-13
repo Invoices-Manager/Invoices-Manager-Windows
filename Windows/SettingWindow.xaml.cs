2f28357846377a6058577468f27f2b8d40a66676
Schecher_1 <Schecher_1@web.de>
2022-10-13 17:10:12 +0200

    edit the about glitch

--- Windows/SettingWindow.xaml.cs
+++ Windows/SettingWindow.xaml.cs
@@ -19,20 +19,18 @@ namespace InvoicesManager.Windows
             foreach (string uiL in EnvironmentsVariable.UILanguages)
                 Comb_UILanguage.Items.Add(uiL);
 
-            Tb_PathData.Text = EnvironmentsVariable.PathData;
             Tb_PathProgram.Text = EnvironmentsVariable.PathPDFBrowser;
             Comb_UILanguage.Text = EnvironmentsVariable.UILanguage;
         }
 
         private void Bttn_SaveSettings_Click(object sender, RoutedEventArgs e)
         {
-            if (Comb_UILanguage.SelectedIndex == -1 || String.IsNullOrWhiteSpace(Tb_PathProgram.Text) || String.IsNullOrWhiteSpace(Tb_PathData.Text))
+            if (Comb_UILanguage.SelectedIndex == -1 || String.IsNullOrWhiteSpace(Tb_PathProgram.Text))
             {
                 MessageBox.Show(Application.Current.Resources["checkYouInput"] as string, Application.Current.Resources["error"] as string);
                 return;
             }
 
-            EnvironmentsVariable.PathData = Tb_PathData.Text;
             EnvironmentsVariable.PathPDFBrowser = Tb_PathProgram.Text;
             EnvironmentsVariable.UILanguage = Comb_UILanguage.Text;
 
