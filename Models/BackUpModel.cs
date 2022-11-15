08e144d501ea64479338e93ecf6abc0f059327c1
Schecher_1 <Schecher_1@web.de>
2022-11-15 21:09:09 +0100

    Update the backup model

--- Models/BackUpModel.cs
+++ Models/BackUpModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InvoicesManager.Classes.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace InvoicesManager.Models
@@ -18,10 +19,17 @@ namespace InvoicesManager.Models
 
     public class SubInvoiceBackUpModel
     {
+        public string FileID { get; set; } 
+        public DateTime CaptureDate { get; set; } 
         public DateTime ExhibitionDate { get; set; }
-        public string Organization { get; set; }
-        public string DocumentType { get; set; }
+        public string Reference { get; set; } 
+        public string DocumentType { get; set; } 
+        public string Organization { get; set; } 
         public string InvoiceNumber { get; set; }
-        public string Reference { get; set; }
+        public string[] Tags { get; set; }
+        public ImportanceStateEnum ImportanceState { get; set; } 
+        public MoneyStateEnum MoneyState { get; set; }
+        public PaidStateEnum PaidState { get; set; } 
+        public double MoneyTotal { get; set; }
     }
 }
