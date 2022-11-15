bb83a3525ce9f486da0164581176d8774599cb26
Schecher_1 <Schecher_1@web.de>
2022-11-15 17:43:58 +0100

    set new invoice model

--- Models/InvoiceModel.cs
+++ Models/InvoiceModel.cs
@@ -5,38 +5,40 @@ namespace InvoicesManager.Models
 {
     public class InvoiceModel
     {
-        public bool ShouldSerializeOpenInvoiceText(){return false;}
-        public bool ShouldSerializeStringExhibitionDate(){return false;}
+        public bool ShouldSerializeOpenInvoiceText() { return false; }
+        public bool ShouldSerializeStringExhibitionDate() { return false; }
 
         public string OpenInvoiceText { get; } = Application.Current.Resources["open"] as string;
-        public DateTime ExhibitionDate { get; set; }
+           
+        public string FileID { get; set; } // (NN)
+        public DateTime CaptureDate { get; set; } // (NN)
+        public DateTime ExhibitionDate { get; set; } // (NN)      
         public string StringExhibitionDate { get { return ExhibitionDate.ToString("yyyy.MM.dd"); } }
-        public string Organization { get; set; }
-        public string DocumentType { get; set; }
+        public string Reference { get; set; } // (NN)
+        public string DocumentType { get; set; } // (NN)
+        public string Organization { get; set; } // (NN)
         public string InvoiceNumber { get; set; }
-        public string Reference { get; set; }
-        public string Path { get; set; }
+        public string[] Tags { get; set; }
+        public ImportanceStateEnum ImportanceState { get; set; } // { VeryImportant, Important, Neutral, Unimportant }
+        public MoneyStateEnum MoneyState { get; set; } // { Paid , Received,  NoInvoice }
+        public PaidStateEnum PaidState { get; set; } // { Paid , Unpaid,  NoInvoice }
+        public double MoneyTotal { get; set; }
     }
 }
 
+//OLD
 
-/*
-public string FileID { get; set; } // (NN)
-public DateTime CaptureDate { get; set; } // (NN)
-public DateTime ExhibitionDate { get; set; } // (NN)
-public string Reference { get; set; } // (NN)
-public string DocumentType { get; set; } // (NN)
-public string Organization { get; set; } // (NN)
-public string InvoiceNumber { get; set; } 
-public string[] Tags { get; set; }
-public ImportanceStateEnum ImportanceState { get; set; } // { VeryImportant, Important, Neutral, Unimportant }
-public MoneyStateEnum MoneyState { get; set; } // { Paid , Received,  NoInvoice }
-public PaidStateEnum PaidState { get; set; } // { Paid , Unpaid,  NoInvoice }
-public double MoneyTotal { get; set; }
+//public class InvoiceModel
+//{
+//    public bool ShouldSerializeOpenInvoiceText() { return false; }
+//    public bool ShouldSerializeStringExhibitionDate() { return false; }
 
-
-
- */
-
-
-
+//    public string OpenInvoiceText { get; } = Application.Current.Resources["open"] as string;
+//    public DateTime ExhibitionDate { get; set; }
+//    public string StringExhibitionDate { get { return ExhibitionDate.ToString("yyyy.MM.dd"); } }
+//    public string Organization { get; set; }
+//    public string DocumentType { get; set; }
+//    public string InvoiceNumber { get; set; }
+//    public string Reference { get; set; }
+//    public string Path { get; set; }
+//}
