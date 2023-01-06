3c2ef34859c3c70f2f760c15294ed5aa48b65ec7
Schecher_1 <Schecher_1@web.de>
2023-01-06 19:01:21 +0100

    code cleanup

--- Classes/HashManager.cs
+++ Classes/HashManager.cs
@@ -6,21 +6,17 @@ namespace InvoicesManager.Classes
     {
         public static string GetMD5HashFromFile(string path)
         {
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                using (var stream = System.IO.File.OpenRead(path))
-                {
-                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
-                }
-            }
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            using var stream = System.IO.File.OpenRead(path);
+            
+            return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
         }
 
         public static string GetMD5HashFromByteArray(byte[] bytes)
         {
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                return BitConverter.ToString(md5.ComputeHash(bytes)).Replace("-", "").ToLowerInvariant();
-            }
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            
+            return BitConverter.ToString(md5.ComputeHash(bytes)).Replace("-", "").ToLowerInvariant();
         }
     }
 }
