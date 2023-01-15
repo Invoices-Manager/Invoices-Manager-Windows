0bf85d888f272b8e5892386cc8fe5feb6a6187d1
Schecher_1 <Schecher_1@web.de>
2023-01-15 12:39:28 +0100

    add the mutex system

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,33 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System;
+using System.Windows;
 
 namespace InvoicesManager
 {
     public partial class App : Application
     {
+        private readonly Mutex InvoiceManagerMutex = new Mutex(true, "{GnM-InvoicesManager-GnM-MadeBySchecher_1}");
+
+        public App()
+        {
+            try
+            {
+                //check if it is already claimed
+                if (!InvoiceManagerMutex.WaitOne(TimeSpan.Zero, true))
+                {
+                    //the app is already running
+                    MessageBox.Show("Another instance of the app is already running.");
+                    return;
+                }
+
+                //init the app
+                InitializeComponent();
+            }
+            finally
+            {
+                //release the mutex
+                InvoiceManagerMutex.ReleaseMutex();
+            }
+        }
     }
 }
