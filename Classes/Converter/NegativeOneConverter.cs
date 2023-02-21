aedcb4eec93ec8169c05df4c0db6f2086cd74b6e
Schecher_1 <Schecher_1@web.de>
2023-02-21 21:28:15 +0100

    added the deleted converter again (is needed)

---
+++ Classes/Converter/NegativeOneConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace InvoicesManager.Classes.Converter
+{
+    public class NegativeOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string i && i == ("-1,00" + EnvironmentsVariable.MoneyUnit))
+                return "";
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
