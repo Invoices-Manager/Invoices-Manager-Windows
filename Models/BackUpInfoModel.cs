9ce76cbc9e26522b47f2b9698c6533c5ab228b3c
Schecher_1 <Schecher_1@web.de>
2023-03-11 18:43:40 +0100

    create backup mgr ui (without logic)

---
+++ Models/BackUpInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace InvoicesManager.Models
+{
+    public class BackUpInfoModel
+    {
+        public DateTime DateOfCreation { get; set; }
+        public string BackUpVersion { get; set; }
+
+        public int EntityCountInvoices { get; set; }
+        public int EntityCountNotebooks { get; set; }
+
+        public string BackUpName { get; set; }
+        public string BackUpPath { get; set; }
+    }
+}
